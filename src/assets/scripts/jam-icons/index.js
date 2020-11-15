287e4c7b53dc70d44215b41e5ddb111e2dc0816e
Yan Sultanov <ivan.uhalin@gmail.com>
2020-11-15 13:32:37 +0300

    Created

---
+++ src/assets/scripts/jam-icons/index.js
@@ -0,0 +1,11 @@
+/**
+ * https://github.com/michaelampr/jam
+ * https://github.com/antonreshetov/vue-unicons
+ */
+
+import JamIcons from './component'
+import * as icons from './icons'
+
+JamIcons.add(Object.values({ ...icons }))
+
+export default JamIcons
---
+++ src/assets/styles/index.js
@@ -0,0 +1,11 @@
+// Base styles
+import './base/colors.scss'
+import './base/fonts.scss'
+import './base/normalize.scss'
+
+// Blocks styles
+import './blocks/btn.scss'
+import './blocks/hd.scss'
+import './blocks/line.scss'
+import './blocks/pr.scss'
+import './blocks/sr-only.scss'
