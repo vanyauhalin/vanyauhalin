fe13b54b7b3afc4250ff7c51688fe67186e2f96c
Yan Sultanov <ivan.uhalin@gmail.com>
2020-11-26 17:35:53 +0300

    Created

---
+++ src/assets/scripts/variables.js
@@ -0,0 +1,14 @@
+export const duration = 0.9
+
+// Variable ($main-br) from variables.scss
+export const border = 4
+
+// Padding ($main-pd-y) from App.vue
+export const padding = 35
+
+// Margin from line.scss
+export const line = {
+  top: 25,
+  bottom: 28,
+  minified: 18.5
+}
